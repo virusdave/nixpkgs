1c2fa842627b43edac36a3e73eae80fcf2e5a9c7
Guilhem Saurel <guilhem.saurel@laas.fr>
2026-01-05 02:23:59 +0100

    python3Packages.safelz4: init at 0.1.0

--- pkgs/top-level/python-packages.nix
+++ pkgs/top-level/python-packages.nix
@@ -16887,6 +16887,8 @@ self: super: with self; {
 
   safeio = callPackage ../development/python-modules/safeio { };
 
+  safelz4 = callPackage ../development/python-modules/safelz4 { };
+
   safetensors = callPackage ../development/python-modules/safetensors { };
 
   safety = callPackage ../development/python-modules/safety { };
