eac03580b6a638f0d3f4a51c34ef26257ca67a8f
Stefan Frijters <sfrijters@gmail.com>
2026-01-30 23:36:14 +0100

    cargo-watch: move NIX_LDFLAGS into env for structuredAttrs

--- pkgs/by-name/ca/cargo-watch/package.nix
+++ pkgs/by-name/ca/cargo-watch/package.nix
@@ -18,10 +18,12 @@ rustPlatform.buildRustPackage rec {
 
   cargoHash = "sha256-4AVZ747d6lOjxHN+co0A7APVB5Xj6g5p/Al5fLbgPnc=";
 
-  NIX_LDFLAGS = lib.optionals (stdenv.hostPlatform.isDarwin && stdenv.hostPlatform.isx86_64) [
-    "-framework"
-    "AppKit"
-  ];
+  env = lib.optionalAttrs (stdenv.hostPlatform.isDarwin && stdenv.hostPlatform.isx86_64) {
+    NIX_LDFLAGS = toString [
+      "-framework"
+      "AppKit"
+    ];
+  };
 
   # `test with_cargo` tries to call cargo-watch as a cargo subcommand
   # (calling cargo-watch with command `cargo watch`)
