475ed21c86157799372c0558d971349599b69fb7
Kylie McClain <kylie@somas.is>
2026-01-31 13:36:00 -0500

    kdotool: fix build using upstream patches

--- pkgs/by-name/kd/kdotool/package.nix
+++ pkgs/by-name/kd/kdotool/package.nix
@@ -1,6 +1,7 @@
 {
   lib,
   fetchFromGitHub,
+  fetchpatch,
   rustPlatform,
   pkg-config,
   dbus,
@@ -16,6 +17,18 @@ rustPlatform.buildRustPackage rec {
     rev = "v${version}";
     hash = "sha256-qx4bWAFQcoLM/r4aNzmoZdjclw8ccAW8lKLda6ON1aQ=";
   };
+
+  patches = [
+    # Remove these two on next release.
+    (fetchpatch {
+      url = "https://github.com/jinliu/kdotool/commit/049e3f5620ad8c5484241d7d06d742bc17d423ed.patch";
+      hash = "sha256-VTpHlT6XMVRgJIeLjxZPHkzaYFZCYtS8IAD0mKZ8rzs=";
+    })
+    (fetchpatch {
+      url = "https://github.com/jinliu/kdotool/commit/e0a3bff3b5d9882033dd72836e5fcff572b64135.patch";
+      hash = "sha256-6IsV9O2h9N/FxGQRHS8qAbEqdr7282ziGza5K52vpPk=";
+    })
+  ];
 
   cargoHash = "sha256-ASR2zMwVCKeEZPYQNoO54J00eZyTn1i6FE0NBCJWSCs=";
 
