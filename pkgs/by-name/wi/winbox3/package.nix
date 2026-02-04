2f293e1fe3ec3dacd14da3d2f95161da4ddcc7b4
hideyosh1 <penelope.zhong@proton.me>
2026-02-04 17:27:11 -0600

    wine: move packages that depend on "wine64" binary to require "wine"
    according to upstream: "The wine64 loader binary is removed, in favor of
    a single wine loader that selects the correct mode based on the binary
    being executed. For binaries that have both 32-bit and 64-bit versions
    installed, it defaults to 64-bit. The 32-bit version can then be
    launched with an explicit path, e.g. wine
    c:\\windows\\syswow64\\notepad.exe.",
    
    I have changed all the references to the wine64 binary to only need the
    wine binary -- tentative.

--- pkgs/by-name/wi/winbox3/package.nix
+++ pkgs/by-name/wi/winbox3/package.nix
@@ -22,15 +22,15 @@ stdenvNoCC.mkDerivation (finalAttrs: {
   version = "3.43";
 
   src = fetchurl (
-    if (wine.meta.mainProgram == "wine64") then
-      {
-        url = "https://download.mikrotik.com/routeros/winbox/${finalAttrs.version}/winbox64.exe";
-        hash = "sha256-W0HPUf2B6NCCaH9rUiFZz0q6IubfjtxIZyHU4JUHtuk=";
-      }
-    else
+    if (builtins.elem "i686-linux" wine.meta.platforms) then
       {
         url = "https://download.mikrotik.com/routeros/winbox/${finalAttrs.version}/winbox.exe";
         hash = "sha256-pAOOTgmjQoXI2o2MKTDuOOpb7q0rb/zWATDNyAMOLms=";
+      }
+    else
+      {
+        url = "https://download.mikrotik.com/routeros/winbox/${finalAttrs.version}/winbox64.exe";
+        hash = "sha256-W0HPUf2B6NCCaH9rUiFZz0q6IubfjtxIZyHU4JUHtuk=";
       }
   );
 
