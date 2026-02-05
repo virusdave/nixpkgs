e8c5b555eb78634b6c92c99d47994a3ea5e3a709
Julio Merino <julio@meroh.net>
2026-02-05 10:26:23 -0800

    maintainers: add jmmv

--- maintainers/maintainer-list.nix
+++ maintainers/maintainer-list.nix
@@ -12536,6 +12536,12 @@
     githubId = 43830312;
     name = "Joël Miramon";
   };
+  jmmv = {
+    email = "julio@meroh.net";
+    github = "jmmv";
+    githubId = 879272;
+    name = "Julio Merino";
+  };
   jn-sena = {
     email = "jn-sena@proton.me";
     github = "jn-sena";
