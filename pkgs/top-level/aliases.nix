bfe0ee25b5567902f08e3c6b7f5266b3b409f741
Jörg Thalheim <joerg@thalheim.io>
2026-02-05 14:33:17 +0100

    carp: remove broken package
    carp has been marked broken since NixOS 24.05 and remains unfixed.
    Per RFC 180, packages broken for a full release cycle are subject
    to removal.
    
    https://github.com/NixOS/rfcs/pull/180

--- pkgs/top-level/aliases.nix
+++ pkgs/top-level/aliases.nix
@@ -407,6 +407,7 @@ mapAliases {
   cargo-espflash = throw "'cargo-espflash' has been renamed to/replaced by 'espflash'"; # Converted to throw 2025-10-27
   cargo-sync-readme = throw "'cargo-sync-readme' has been removed because upstream 404s"; # Converted to throw 2025-12-18
   cargonode = throw "'cargonode' has been removed due to lack of upstream maintenance"; # Added 2025-06-18
+  carp = throw "'carp' has been removed due to being broken for more than a year; see RFC 180"; # Added 2026-02-05
   cask = throw "'cask' has been renamed to/replaced by 'emacs.pkgs.cask'"; # Converted to throw 2025-10-27
   catalyst-browser = throw "'catalyst-browser' has been removed due to a lack of maintenance and not satisfying our security criteria for browsers."; # Added 2025-06-25
   cataract = throw "'cataract' has been removed due to a lack of maintenace"; # Added 2025-08-25
