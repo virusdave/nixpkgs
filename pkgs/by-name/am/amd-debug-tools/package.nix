6b349a6b1b26c001910226355b96174f20e47f70
c6rg0 <gdrozbik08@gmail.com>
2026-02-05 23:26:13 +0000

    amd-debug-tools: 0.2.10 -> 0.2.13

--- pkgs/by-name/am/amd-debug-tools/package.nix
+++ pkgs/by-name/am/amd-debug-tools/package.nix
@@ -9,13 +9,13 @@
 
 python3Packages.buildPythonApplication rec {
   pname = "amd-debug-tools";
-  version = "0.2.10";
+  version = "0.2.13";
   pyproject = true;
 
   src = fetchgit {
     url = "https://git.kernel.org/pub/scm/linux/kernel/git/superm1/amd-debug-tools.git";
     rev = version;
-    hash = "sha256-tbykQ8tc6YKHjKEHA9Ml7Z7MjDQzMGXtTwMG9buiovg=";
+    hash = "sha256-748K4Ee9HVYWQ7/DVz7F2nZNjau5v4OGvgHwJZ4vYpM=";
   };
 
   build-system = with python3Packages; [
