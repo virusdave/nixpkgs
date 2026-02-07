6be684b80e0db0ae3d75b3d4599fa216f94d7664
niri81 <38499069+niri81@users.noreply.github.com>
2026-02-07 15:53:18 +0100

    diesel-cli-ext: 0.3.13 -> 0.3.17

--- pkgs/by-name/di/diesel-cli-ext/package.nix
+++ pkgs/by-name/di/diesel-cli-ext/package.nix
@@ -6,15 +6,15 @@
 
 rustPlatform.buildRustPackage (finalAttrs: {
   pname = "diesel-cli-ext";
-  version = "0.3.13";
+  version = "0.3.17";
 
   src = fetchCrate {
     pname = "diesel_cli_ext";
     inherit (finalAttrs) version;
-    hash = "sha256-5AIzMxEcxL/vYWx3D/meA///Zo+1210HUMEE4dFBhkc=";
+    hash = "sha256-RkRnkr0xwe1ruQwGa1105CIgQl0cbRCvsJrCW/pWXDE=";
   };
 
-  cargoHash = "sha256-AvoyJPh59fZPDcOtIZ4UFUgW83szBC3HOOlkxA3VFgE=";
+  cargoHash = "sha256-l1nptYgDlMXmyhH3OC8D5ViuOG+ePSkxNOtcwuXBQIM=";
 
   meta = {
     description = "Provides different tools for projects using the diesel_cli";
