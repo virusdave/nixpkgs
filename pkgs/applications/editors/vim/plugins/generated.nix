adfb1fd3ed369f399c3e9d5a291af2c8f9f6fed6
jaredmontoya <49511278+jaredmontoya@users.noreply.github.com>
2026-02-07 12:03:56 +0100

    vimPlugins.nvim-silicon: init at 1.0.0-unstable-2025-01-09
    https://github.com/michaelrommel/nvim-silicon

--- pkgs/applications/editors/vim/plugins/generated.nix
+++ pkgs/applications/editors/vim/plugins/generated.nix
@@ -11929,6 +11929,19 @@ final: prev: {
     meta.hydraPlatforms = [ ];
   };
 
+  nvim-silicon = buildVimPlugin {
+    pname = "nvim-silicon";
+    version = "1.0.0-unstable-2025-01-09";
+    src = fetchFromGitHub {
+      owner = "michaelrommel";
+      repo = "nvim-silicon";
+      rev = "7f66bda8f60c97a5bf4b37e5b8acb0e829ae3c32";
+      hash = "sha256-XiYn/L2e/B+6LTjak3jAwRgnZ3gCbsyA0J61Dd+jZv4=";
+    };
+    meta.homepage = "https://github.com/michaelrommel/nvim-silicon/";
+    meta.hydraPlatforms = [ ];
+  };
+
   nvim-snippets = buildVimPlugin {
     pname = "nvim-snippets";
     version = "1.0.0-unstable-2024-07-10";
