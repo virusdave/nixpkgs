9ba3ba1678fe1ab593e08425d9c3071793f7c7b5
pancaek <20342389+pancaek@users.noreply.github.com>
2026-02-08 11:58:25 -0800

    mate.libmateweather: move to top-level

--- pkgs/desktops/mate/default.nix
+++ pkgs/desktops/mate/default.nix
@@ -25,7 +25,6 @@ let
       caja-dropbox = callPackage ./caja-dropbox { };
       caja-extensions = callPackage ./caja-extensions { };
       caja-with-extensions = callPackage ./caja/with-extensions.nix { };
-      libmateweather = callPackage ./libmateweather { };
       marco = callPackage ./marco { };
       mate-applets = callPackage ./mate-applets { };
       mate-backgrounds = callPackage ./mate-backgrounds { };
@@ -128,4 +127,5 @@ lib.makeScope pkgs.newScope packages
   eom = lib.warnOnInstantiate "‘mate.eom’ was moved to top-level. Please use ‘pkgs.eom’ directly" pkgs.eom; # Added on 2026-02-08
   libmatekbd = lib.warnOnInstantiate "‘mate.libmatekbd’ was moved to top-level. Please use ‘pkgs.libmatekbd’ directly" pkgs.libmatekbd; # Added on 2026-02-08
   libmatemixer = lib.warnOnInstantiate "‘mate.libmatemixer’ was moved to top-level. Please use ‘pkgs.libmatemixer’ directly" pkgs.libmatemixer; # Added on 2026-02-08
+  libmateweather = lib.warnOnInstantiate "‘mate.libmateweather’ was moved to top-level. Please use ‘pkgs.libmateweather’ directly" pkgs.libmateweather; # Added on 2026-02-08
 }
