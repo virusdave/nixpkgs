ae610270b0a83e282028801b5b755d54273622ab
pancaek <20342389+pancaek@users.noreply.github.com>
2026-02-08 12:28:55 -0800

    mate.mate-user-guide: move to top-level

--- pkgs/desktops/mate/default.nix
+++ pkgs/desktops/mate/default.nix
@@ -30,7 +30,6 @@ let
       mate-settings-daemon = callPackage ./mate-settings-daemon { };
       mate-settings-daemon-wrapped = callPackage ./mate-settings-daemon/wrapped.nix { };
 
-      mate-user-guide = callPackage ./mate-user-guide { };
       mate-user-share = callPackage ./mate-user-share { };
       mate-utils = callPackage ./mate-utils { };
       mate-wayland-session = callPackage ./mate-wayland-session { };
@@ -128,4 +127,5 @@ lib.makeScope pkgs.newScope packages
   mate-terminal = lib.warnOnInstantiate "‘mate.mate-terminal’ was moved to top-level. Please use ‘pkgs.mate-terminal’ directly" pkgs.mate-terminal; # Added on 2026-02-08
   mate-themes = lib.warnOnInstantiate "‘mate.mate-themes’ was moved to top-level. Please use ‘pkgs.mate-themes’ directly" pkgs.mate-themes; # Added on 2026-02-08
   mate-tweak = lib.warnOnInstantiate "‘mate.mate-tweak’ was moved to top-level. Please use ‘pkgs.mate-tweak’ directly" pkgs.mate-tweak; # Added on 2026-02-08
+  mate-user-guide = lib.warnOnInstantiate "‘mate.mate-user-guide’ was moved to top-level. Please use ‘pkgs.mate-user-guide’ directly" pkgs.mate-user-guide; # Added on 2026-02-08
 }
