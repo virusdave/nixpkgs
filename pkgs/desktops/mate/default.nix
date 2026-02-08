429c04e13750155e7f7ea262db478bcfa80f28d1
pancaek <20342389+pancaek@users.noreply.github.com>
2026-02-08 12:30:57 -0800

    mate.mate-wayland-session: move to top-level

--- pkgs/desktops/mate/default.nix
+++ pkgs/desktops/mate/default.nix
@@ -30,7 +30,6 @@ let
       mate-settings-daemon = callPackage ./mate-settings-daemon { };
       mate-settings-daemon-wrapped = callPackage ./mate-settings-daemon/wrapped.nix { };
 
-      mate-wayland-session = callPackage ./mate-wayland-session { };
       mozo = callPackage ./mozo { };
       pluma = callPackage ./pluma { };
       python-caja = callPackage ./python-caja { };
@@ -128,4 +127,5 @@ lib.makeScope pkgs.newScope packages
   mate-user-guide = lib.warnOnInstantiate "‘mate.mate-user-guide’ was moved to top-level. Please use ‘pkgs.mate-user-guide’ directly" pkgs.mate-user-guide; # Added on 2026-02-08
   mate-user-share = lib.warnOnInstantiate "‘mate.mate-user-share’ was moved to top-level. Please use ‘pkgs.mate-user-share’ directly" pkgs.mate-user-share; # Added on 2026-02-08
   mate-utils = lib.warnOnInstantiate "‘mate.mate-utils’ was moved to top-level. Please use ‘pkgs.mate-utils’ directly" pkgs.mate-utils; # Added on 2026-02-08
+  mate-wayland-session = lib.warnOnInstantiate "‘mate.mate-wayland-session’ was moved to top-level. Please use ‘pkgs.mate-wayland-session’ directly" pkgs.mate-wayland-session; # Added on 2026-02-08
 }
