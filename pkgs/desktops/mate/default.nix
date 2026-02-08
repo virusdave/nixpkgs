05f36948dd329d10c075a0cd639163fa03758293
pancaek <20342389+pancaek@users.noreply.github.com>
2026-02-08 11:59:19 -0800

    mate.marco: move to top-level

--- pkgs/desktops/mate/default.nix
+++ pkgs/desktops/mate/default.nix
@@ -25,7 +25,6 @@ let
       caja-dropbox = callPackage ./caja-dropbox { };
       caja-extensions = callPackage ./caja-extensions { };
       caja-with-extensions = callPackage ./caja/with-extensions.nix { };
-      marco = callPackage ./marco { };
       mate-applets = callPackage ./mate-applets { };
       mate-backgrounds = callPackage ./mate-backgrounds { };
       mate-calc = callPackage ./mate-calc { };
@@ -128,4 +127,5 @@ lib.makeScope pkgs.newScope packages
   libmatekbd = lib.warnOnInstantiate "‘mate.libmatekbd’ was moved to top-level. Please use ‘pkgs.libmatekbd’ directly" pkgs.libmatekbd; # Added on 2026-02-08
   libmatemixer = lib.warnOnInstantiate "‘mate.libmatemixer’ was moved to top-level. Please use ‘pkgs.libmatemixer’ directly" pkgs.libmatemixer; # Added on 2026-02-08
   libmateweather = lib.warnOnInstantiate "‘mate.libmateweather’ was moved to top-level. Please use ‘pkgs.libmateweather’ directly" pkgs.libmateweather; # Added on 2026-02-08
+  marco = lib.warnOnInstantiate "‘mate.marco’ was moved to top-level. Please use ‘pkgs.marco’ directly" pkgs.marco; # Added on 2026-02-08
 }
