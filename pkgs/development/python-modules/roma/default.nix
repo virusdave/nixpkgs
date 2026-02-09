3378dd818095bac4e2b4fc312d7d78731f5255ec
Guilhem Saurel <guilhem.saurel@laas.fr>
2026-02-09 09:34:39 +0100

    python3Packages.roma: rec -> finalAttrs + changelog

--- pkgs/development/python-modules/roma/default.nix
+++ pkgs/development/python-modules/roma/default.nix
@@ -8,7 +8,7 @@
   torch,
 }:
 
-buildPythonPackage rec {
+buildPythonPackage (finalAttrs: {
   pname = "roma";
   version = "1.5.5";
   pyproject = true;
@@ -16,7 +16,7 @@ buildPythonPackage rec {
   src = fetchFromGitHub {
     owner = "naver";
     repo = "roma";
-    tag = "v${version}";
+    tag = "v${finalAttrs.version}";
     hash = "sha256-0R8p8pQxLQqK7MTbk9J5lFtA13XJth76Glemkfj9X/E=";
   };
 
@@ -38,9 +38,10 @@ buildPythonPackage rec {
   ];
 
   meta = {
+    changelog = "https://naver.github.io/roma/#changelog";
     description = "Lightweight library to deal with 3D rotations in PyTorch";
     homepage = "https://github.com/naver/roma";
     license = lib.licenses.bsd3;
     maintainers = with lib.maintainers; [ nim65s ];
   };
-}
+})
