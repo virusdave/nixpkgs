6d51dde52cb0128e79961a106055982046181721
Fabian Affolter <mail@fabian-affolter.ch>
2026-02-09 09:57:18 +0100

    python313Packages.tplink-omada-client: migrate to finalAttrs

--- pkgs/development/python-modules/tplink-omada-client/default.nix
+++ pkgs/development/python-modules/tplink-omada-client/default.nix
@@ -7,14 +7,14 @@
   hatchling,
 }:
 
-buildPythonPackage rec {
+buildPythonPackage (finalAttrs: {
   pname = "tplink-omada-client";
   version = "1.5.5";
   pyproject = true;
 
   src = fetchPypi {
     pname = "tplink_omada_client";
-    inherit version;
+    inherit (finalAttrs) version;
     hash = "sha256-uefZ1MIt0xuLo1JZEYIJhrwY4V8KG0Rb16+ZY7PwEwI=";
   };
 
@@ -33,9 +33,9 @@ buildPythonPackage rec {
   meta = {
     description = "Library for the TP-Link Omada SDN Controller API";
     homepage = "https://github.com/MarkGodwin/tplink-omada-api";
-    changelog = "https://github.com/MarkGodwin/tplink-omada-api/releases/tag/release%2Fv${version}";
+    changelog = "https://github.com/MarkGodwin/tplink-omada-api/releases/tag/release%2Fv${finalAttrs.version}";
     license = lib.licenses.mit;
     maintainers = with lib.maintainers; [ fab ];
     mainProgram = "omada";
   };
-}
+})
