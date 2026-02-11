7aeade0968490d3ed9df50f66f8255c75eba2cce
Laurent Le Houerou <laurent@lehouerou.net>
2026-02-11 21:55:04 +0400

    maintainers: add llehouerou

--- maintainers/maintainer-list.nix
+++ maintainers/maintainer-list.nix
@@ -15210,6 +15210,12 @@
     githubId = 78693624;
     name = "llakala";
   };
+  llehouerou = {
+    email = "laurent@lehouerou.net";
+    github = "llehouerou";
+    githubId = 3669155;
+    name = "Laurent Le Houerou";
+  };
   lluchs = {
     email = "lukas.werling@gmail.com";
     github = "lluchs";
