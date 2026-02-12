7f1d955625ec1ed035ce3110f5e2106787ea62e1
quantenzitrone <general@dev.quantenzitrone.eu>
2026-02-12 04:43:32 +0100

    twitterBootstrap: refactor
    the output doesn't change

--- pkgs/by-name/tw/twitterBootstrap/package.nix
+++ pkgs/by-name/tw/twitterBootstrap/package.nix
@@ -1,30 +1,13 @@
 {
   lib,
-  stdenv,
-  fetchurl,
-  unzip,
+  fetchzip,
 }:
-
-stdenv.mkDerivation (finalAttrs: {
+fetchzip (finalAttrs: {
   pname = "bootstrap";
   version = "5.3.8";
 
-  src = fetchurl {
-    url = "https://github.com/twbs/bootstrap/releases/download/v${finalAttrs.version}/bootstrap-${finalAttrs.version}-dist.zip";
-    hash = "sha256-MljIc8vLHi2B9DdK/qLqZDfZ7ukHcEEHP9gd1XnFums=";
-  };
-
-  nativeBuildInputs = [ unzip ];
-
-  dontBuild = true;
-  installPhase = ''
-    runHook preInstall
-
-    mkdir $out
-    cp -r * $out/
-
-    runHook postInstall
-  '';
+  url = "https://github.com/twbs/bootstrap/releases/download/v${finalAttrs.version}/bootstrap-${finalAttrs.version}-dist.zip";
+  hash = "sha256-StRhHJIRGzguLlo0BGOAMy0PCCmMovzgU/5xZJgVrqQ=";
 
   meta = {
     description = "Front-end framework for faster and easier web development";
