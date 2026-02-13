b8ace9a5e3658d39c9cfdc718b537bc70afd54e4
Hamid <hamidr.dev@gmail.com>
2026-02-13 15:00:48 +0100

    python3Packages.real-ladybug: init at 0.14.1

--- pkgs/top-level/python-packages.nix
+++ pkgs/top-level/python-packages.nix
@@ -16330,6 +16330,8 @@ self: super: with self; {
 
   readthedocs-sphinx-ext = callPackage ../development/python-modules/readthedocs-sphinx-ext { };
 
+  real-ladybug = callPackage ../development/python-modules/real-ladybug { };
+
   realtime = callPackage ../development/python-modules/realtime { };
 
   rebulk = callPackage ../development/python-modules/rebulk { };
