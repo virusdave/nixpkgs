b10e3bac24af1bf6b1a12b748c4d406297da52c3
Ryan Omasta <git@ryand.ca>
2026-02-15 02:03:23 -0700

    python3Packages.requests-hardened: init at 1.2.0

--- pkgs/top-level/python-packages.nix
+++ pkgs/top-level/python-packages.nix
@@ -16488,6 +16488,8 @@ self: super: with self; {
 
   requests-gssapi = callPackage ../development/python-modules/requests-gssapi { };
 
+  requests-hardened = callPackage ../development/python-modules/requests-hardened { };
+
   requests-hawk = callPackage ../development/python-modules/requests-hawk { };
 
   requests-http-message-signatures =
