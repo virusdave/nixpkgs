84710b296e8320035ba0abd69581315fd5bf9385
Fabian Affolter <mail@fabian-affolter.ch>
2026-02-15 21:38:22 +0100

    python314Packages.powerfox: migrate to finalAttrs

--- pkgs/development/python-modules/powerfox/default.nix
+++ pkgs/development/python-modules/powerfox/default.nix
@@ -14,7 +14,7 @@
   yarl,
 }:
 
-buildPythonPackage rec {
+buildPythonPackage (finalAttrs: {
   pname = "powerfox";
   version = "2.1.0";
   pyproject = true;
@@ -22,7 +22,7 @@ buildPythonPackage rec {
   src = fetchFromGitHub {
     owner = "klaasnicolaas";
     repo = "python-powerfox";
-    tag = "v${version}";
+    tag = "v${finalAttrs.version}";
     hash = "sha256-JsvLa5zZ6E+d5l4sIngp0KvZlN8BgBxllk9Md19kZLY=";
   };
 
@@ -48,8 +48,8 @@ buildPythonPackage rec {
   meta = {
     description = "Asynchronous Python client for the Powerfox devices";
     homepage = "https://github.com/klaasnicolaas/python-powerfox";
-    changelog = "https://github.com/klaasnicolaas/python-powerfox/releases/tag/${src.tag}";
+    changelog = "https://github.com/klaasnicolaas/python-powerfox/releases/tag/${finalAttrs.src.tag}";
     license = lib.licenses.mit;
     maintainers = with lib.maintainers; [ fab ];
   };
-}
+})
