335f2be3c58c3f4fdee380b3a97658c27bcfc9db
jopejoe1 <jopejoe1@missing.ninja>
2026-02-16 10:08:36 +0100

    treewide: replace `lib.trivial.version` with `lib.trivial.release`
    includes a fix that will be reverted in staging

--- pkgs/build-support/trivial-builders/default.nix
+++ pkgs/build-support/trivial-builders/default.nix
@@ -774,7 +774,7 @@ rec {
           # turn off __structuredAttrs to unbreak substituteAll
           __structuredAttrs = false;
           pname = name;
-          inherit (lib.trivial) version;
+          version = lib.trivial.release + "pre-git";
           inherit meta;
           inherit depsTargetTargetPropagated;
           inherit propagatedBuildInputs;
