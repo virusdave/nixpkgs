b9e5ff666335ff8178274dde10d7234592948493
Coca <me@coca.codes>
2026-02-16 06:15:22 +0000

    space-station-14-launcher: minor refactors

--- pkgs/by-name/sp/space-station-14-launcher/package.nix
+++ pkgs/by-name/sp/space-station-14-launcher/package.nix
@@ -36,14 +36,17 @@
   # Path to set ROBUST_SOUNDFONT_OVERRIDE to, essentially the default soundfont used.
   soundfont-path ? "${soundfont-fluid}/share/soundfonts/FluidR3_GM2-2.sf2",
 }:
-buildDotnetModule rec {
+let
   pname = "space-station-14-launcher";
   version = "0.37.1";
+in
+buildDotnetModule rec {
+  inherit pname;
 
   # Workaround to prevent buildDotnetModule from overriding assembly versions.
-  name = "space-station-14-launcher-${version}";
+  # If you inherit version it will break loading Robust.LoaderApi when connecting to a server!
+  name = "${pname}-${version}";
 
-  # A bit redundant but I don't trust this package to be maintained by anyone else.
   src = fetchFromGitHub {
     owner = "space-wizards";
     repo = "SS14.Launcher";
@@ -111,9 +114,9 @@ buildDotnetModule rec {
 
   desktopItems = [
     (makeDesktopItem {
-      name = "space-station-14-launcher";
+      name = pname;
       exec = meta.mainProgram;
-      icon = "space-station-14-launcher";
+      icon = pname;
       desktopName = "Space Station 14 Launcher";
       comment = meta.description;
       categories = [ "Game" ];
@@ -125,7 +128,7 @@ buildDotnetModule rec {
     mkdir -p $out/lib/space-station-14-launcher/loader
     cp -r SS14.Loader/bin/${buildType}/*/*/* $out/lib/space-station-14-launcher/loader/
 
-    icoFileToHiColorTheme SS14.Launcher/Assets/icon.ico space-station-14-launcher $out
+    icoFileToHiColorTheme SS14.Launcher/Assets/icon.ico ${pname} $out
   '';
 
   meta = {
