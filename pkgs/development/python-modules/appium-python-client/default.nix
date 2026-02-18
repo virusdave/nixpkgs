44aa0ec866041ce0197bef308f8abd34accc27ce
Fabian Affolter <mail@fabian-affolter.ch>
2026-02-18 10:16:23 +0100

    python3Packages.appium-python-client: modernize
    - add changelog to meta
    - migrate to finalAttrs

--- pkgs/development/python-modules/appium-python-client/default.nix
+++ pkgs/development/python-modules/appium-python-client/default.nix
@@ -6,7 +6,7 @@
   selenium,
 }:
 
-buildPythonPackage rec {
+buildPythonPackage (finalAttrs: {
   pname = "appium-python-client";
   version = "5.2.6";
   pyproject = true;
@@ -14,22 +14,21 @@ buildPythonPackage rec {
   src = fetchFromGitHub {
     owner = "appium";
     repo = "python-client";
-    tag = "v${version}";
-    sha256 = "sha256-BTbz2ncCl6C2QBCLMaIZn4fv/ib/IvkWoRSrlxuFauM=";
+    tag = "v${finalAttrs.version}";
+    hash = "sha256-BTbz2ncCl6C2QBCLMaIZn4fv/ib/IvkWoRSrlxuFauM=";
   };
 
   build-system = [ hatchling ];
 
-  dependencies = [
-    selenium
-  ];
+  dependencies = [ selenium ];
 
   pythonImportsCheck = [ "appium" ];
 
   meta = {
     description = "Cross-platform automation framework for all kinds of apps, built on top of the W3C WebDriver protocol";
     homepage = "https://appium.io/";
+    changelog = "https://github.com/appium/python-client/releases/tag/${finalAttrs.src.tag}";
     license = lib.licenses.asl20;
     maintainers = with lib.maintainers; [ eyjhb ];
   };
-}
+})
