55b2f780b0625066d5723808c9e584357b4208bf
quantenzitrone <general@dev.quantenzitrone.eu>
2026-02-18 22:32:20 +0100

    prometheus-json-exporter: move to pkgs/by-name
    this shouldn't create any rebuilds

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -8524,7 +8524,6 @@ with pkgs;
   postgres-websockets = haskellPackages.postgres-websockets.bin;
   postgrest = haskellPackages.postgrest.bin;
 
-  prometheus-json-exporter = callPackage ../servers/monitoring/prometheus/json-exporter.nix { };
   prometheus-junos-czerwonk-exporter =
     callPackage ../servers/monitoring/prometheus/junos-czerwonk-exporter.nix
       { };
