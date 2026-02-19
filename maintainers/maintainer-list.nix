9a4b36ba5f8229016a551d29dc6e3ea9643687df
sudo-mac <sudo-mac@users.noreply.github.com>
2026-02-19 03:18:38 -0500

    maintainers: add sudo-mac
    Signed-off-by: sudo-mac <sudo-mac@users.noreply.github.com>

--- maintainers/maintainer-list.nix
+++ maintainers/maintainer-list.nix
@@ -25542,6 +25542,12 @@
     githubId = 12984845;
     name = "Subhrajyoti Sen";
   };
+  sudo-mac = {
+    email = "dreems2reality@gmail.com";
+    github = "sudo-mac";
+    githubId = 90754995;
+    name = "sudo-mac";
+  };
   sudoforge = {
     github = "sudoforge";
     githubId = 3893293;
