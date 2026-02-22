ee78da9d9987c766e88f29d29f46f812ffeb9dfc
Doron Behar <doron.behar@gmail.com>
2026-02-23 00:07:23 +0200

    python3.pkgs.fastlite: init at 0.2.4

--- pkgs/top-level/python-packages.nix
+++ pkgs/top-level/python-packages.nix
@@ -5357,6 +5357,8 @@ self: super: with self; {
 
   fastjsonschema = callPackage ../development/python-modules/fastjsonschema { };
 
+  fastlite = callPackage ../development/python-modules/fastlite { };
+
   fastmcp = callPackage ../development/python-modules/fastmcp { };
 
   fastmri = callPackage ../development/python-modules/fastmri { };
