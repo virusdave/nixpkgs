66c533e3d05ce3b6f7042c9e26eae2e9e800906a
Bjørn Forsman <bjorn.forsman@gmail.com>
2026-02-22 08:54:05 +0100

    sops: 3.12.0 -> 3.12.1
    Changelog: https://github.com/getsops/sops/blob/main/CHANGELOG.md#3121

--- pkgs/by-name/so/sops/package.nix
+++ pkgs/by-name/so/sops/package.nix
@@ -12,13 +12,13 @@
 
 buildGoModule (finalAttrs: {
   pname = "sops";
-  version = "3.12.0";
+  version = "3.12.1";
 
   src = fetchFromGitHub {
     owner = "getsops";
     repo = finalAttrs.pname;
     tag = "v${finalAttrs.version}";
-    hash = "sha256-fmOBEu/WnDyk6Nd4vwAs9Qdr4XlXNGJPX9bX9JymEWA=";
+    hash = "sha256-65M6rv4dE2edm+k0mPDZkrOb/LY1Cyi4y1D7xaoOdfw=";
   };
 
   vendorHash = "sha256-dniVUk/PsF111UCujFY6zIKR1cp8c8ZawH2ywMef9hE=";
