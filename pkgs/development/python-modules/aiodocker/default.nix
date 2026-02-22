96cfda5c53c5782530bd42917bad14035e8cd06c
R. Ryantm <ryantm-bot@ryantm.com>
2026-02-22 05:49:16 +0000

    python3Packages.aiodocker: 0.25.0 -> 0.26.0

--- pkgs/development/python-modules/aiodocker/default.nix
+++ pkgs/development/python-modules/aiodocker/default.nix
@@ -9,14 +9,14 @@
 
 buildPythonPackage rec {
   pname = "aiodocker";
-  version = "0.25.0";
+  version = "0.26.0";
   pyproject = true;
 
   src = fetchFromGitHub {
     owner = "aio-libs";
     repo = "aiodocker";
     tag = "v${version}";
-    hash = "sha256-SaPTMpMljAh/6Km/JrbEjAOm30gBHH2QBkj7At/BTBA=";
+    hash = "sha256-XpHEgBmcxYoOlzP16BtVOtfuqb+wj0LN0KxXj7p2atk=";
   };
 
   build-system = [
