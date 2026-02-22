ddbd8cd9e4812e9200d648c53175e3ba4921743d
Fabian Affolter <mail@fabian-affolter.ch>
2026-02-22 16:04:51 +0100

    python314Packages.aiodocker: migrate to finalAttrs

--- pkgs/development/python-modules/aiodocker/default.nix
+++ pkgs/development/python-modules/aiodocker/default.nix
@@ -7,7 +7,7 @@
   aiohttp,
 }:
 
-buildPythonPackage rec {
+buildPythonPackage (finalAttrs: {
   pname = "aiodocker";
   version = "0.26.0";
   pyproject = true;
@@ -15,7 +15,7 @@ buildPythonPackage rec {
   src = fetchFromGitHub {
     owner = "aio-libs";
     repo = "aiodocker";
-    tag = "v${version}";
+    tag = "v${finalAttrs.version}";
     hash = "sha256-XpHEgBmcxYoOlzP16BtVOtfuqb+wj0LN0KxXj7p2atk=";
   };
 
@@ -34,10 +34,10 @@ buildPythonPackage rec {
   pythonImportsCheck = [ "aiodocker" ];
 
   meta = {
-    changelog = "https://github.com/aio-libs/aiodocker/releases/tag/${src.tag}";
+    changelog = "https://github.com/aio-libs/aiodocker/releases/tag/${finalAttrs.src.tag}";
     description = "Docker API client for asyncio";
     homepage = "https://github.com/aio-libs/aiodocker";
     license = lib.licenses.asl20;
     maintainers = with lib.maintainers; [ emilytrau ];
   };
-}
+})
