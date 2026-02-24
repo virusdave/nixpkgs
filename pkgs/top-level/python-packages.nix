2e9997b1a551760c497fdb05504cbc04ea8e6cff
Michael Hoang <enzime@users.noreply.github.com>
2026-02-24 22:30:40 +0100

    python3Packages.pycdlib: init at 1.15.0

--- pkgs/top-level/python-packages.nix
+++ pkgs/top-level/python-packages.nix
@@ -13199,6 +13199,8 @@ self: super: with self; {
 
   pycdio = callPackage ../development/python-modules/pycdio { };
 
+  pycdlib = callPackage ../development/python-modules/pycdlib { };
+
   pycec = callPackage ../development/python-modules/pycec { };
 
   pycep-parser = callPackage ../development/python-modules/pycep-parser { };
