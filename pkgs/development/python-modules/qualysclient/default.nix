d85561e0ff18ffc0af4a2e51d117b04228f2d49d
Fabian Affolter <mail@fabian-affolter.ch>
2026-02-26 09:59:19 +0100

    python3Packages.qualysclient: migrate to finalAttrs

--- pkgs/development/python-modules/qualysclient/default.nix
+++ pkgs/development/python-modules/qualysclient/default.nix
@@ -14,7 +14,7 @@
   urllib3,
 }:
 
-buildPythonPackage rec {
+buildPythonPackage (finalAttrs: {
   pname = "qualysclient";
   version = "0.0.4.8.4";
   pyproject = true;
@@ -22,13 +22,13 @@ buildPythonPackage rec {
   src = fetchFromGitHub {
     owner = "woodtechie1428";
     repo = "qualysclient";
-    tag = "v${version}";
+    tag = "v${finalAttrs.version}";
     hash = "sha256-2m/WHxkomHBudWpFpsgXHN8n+hfLU+lf9fvxhh/3HjA=";
   };
 
   postPatch = ''
     substituteInPlace setup.py \
-      --replace-fail "version=__version__," 'version="${version}",'
+      --replace-fail "version=__version__," 'version="${finalAttrs.version}",'
   '';
 
   build-system = [ setuptools ];
@@ -53,8 +53,8 @@ buildPythonPackage rec {
   meta = {
     description = "Python SDK for interacting with the Qualys API";
     homepage = "https://qualysclient.readthedocs.io/";
-    changelog = "https://github.com/woodtechie1428/qualysclient/releases/tag/v${version}";
+    changelog = "https://github.com/woodtechie1428/qualysclient/releases/tag/${finalAttrs.src.tag}";
     license = lib.licenses.mit;
     maintainers = with lib.maintainers; [ fab ];
   };
-}
+})
