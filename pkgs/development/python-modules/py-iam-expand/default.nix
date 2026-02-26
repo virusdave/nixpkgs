09663bcc72704f7a24931e3ddc22be10d6cadf28
Fabian Affolter <mail@fabian-affolter.ch>
2026-02-26 09:24:22 +0100

    python3Packages.py-iam-expand: migrate to finalAttrs

--- pkgs/development/python-modules/py-iam-expand/default.nix
+++ pkgs/development/python-modules/py-iam-expand/default.nix
@@ -7,7 +7,7 @@
   pytestCheckHook,
 }:
 
-buildPythonPackage rec {
+buildPythonPackage (finalAttrs: {
   pname = "py-iam-expand";
   version = "0.3.0";
   pyproject = true;
@@ -15,7 +15,7 @@ buildPythonPackage rec {
   src = fetchFromGitHub {
     owner = "prowler-cloud";
     repo = "py-iam-expand";
-    tag = version;
+    tag = finalAttrs.version;
     hash = "sha256-qe3eph3bvVy6Yql76e/OecHAXggp/KNLG1k0iWy4K1w=";
   };
 
@@ -30,8 +30,8 @@ buildPythonPackage rec {
   meta = {
     description = "Module to expand and deobfuscate AWS IAM actions";
     homepage = "https://github.com/prowler-cloud/py-iam-expand";
-    changelog = "https://github.com/prowler-cloud/py-iam-expand/releases/tag/${src.tag}";
+    changelog = "https://github.com/prowler-cloud/py-iam-expand/releases/tag/${finalAttrs.src.tag}";
     license = lib.licenses.asl20;
     maintainers = with lib.maintainers; [ fab ];
   };
-}
+})
