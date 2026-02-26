40970e914a192f9942fe12a38f419375726bcfa8
Fabian Affolter <mail@fabian-affolter.ch>
2026-02-26 09:57:41 +0100

    python3Packages.firecrawl-py: migrate to finalAttrs

--- pkgs/development/python-modules/firecrawl-py/default.nix
+++ pkgs/development/python-modules/firecrawl-py/default.nix
@@ -12,7 +12,7 @@
   websockets,
 }:
 
-buildPythonPackage rec {
+buildPythonPackage (finalAttrs: {
   pname = "firecrawl-py";
   version = "2.8.0";
   pyproject = true;
@@ -20,11 +20,11 @@ buildPythonPackage rec {
   src = fetchFromGitHub {
     owner = "mendableai";
     repo = "firecrawl";
-    tag = "v${version}";
+    tag = "v${finalAttrs.version}";
     hash = "sha256-7dB3jdp5jkRiNx63C5sjs3t85fuz5vzurfvYY5jWQyU=";
   };
 
-  sourceRoot = "${src.name}/apps/python-sdk";
+  sourceRoot = "${finalAttrs.src.name}/apps/python-sdk";
 
   build-system = [ setuptools ];
 
@@ -46,8 +46,8 @@ buildPythonPackage rec {
   meta = {
     description = "Turn entire websites into LLM-ready markdown or structured data. Scrape, crawl and extract with a single API";
     homepage = "https://firecrawl.dev";
-    changelog = "https://github.com/mendableai/firecrawl/releases/tag/${src.tag}";
+    changelog = "https://github.com/mendableai/firecrawl/releases/tag/${finalAttrs.src.tag}";
     license = lib.licenses.mit;
     maintainers = [ ];
   };
-}
+})
